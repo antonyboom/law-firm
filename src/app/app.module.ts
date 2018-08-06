832a906f32216671319d6ea6522edd899b61529d
Anton <antony.blinov@gmail.com>
2018-08-06 13:16:05 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,38 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {
+  MatCardModule, MatGridListModule,
+  MatToolbarModule
+} from '@angular/material';
+import { HeaderComponent } from './ui/header/header.component';
+import { MainComponent } from './ui/main/main.component';
+import { FooterComponent } from './ui/footer/footer.component';
+import {AppRoutingModule} from './app-routing.module';
+import { HomeComponent } from './ui/main/home/home.component';
+import { ServicesComponent } from './ui/main/services/services.component';
+import { AboutComponent } from './ui/main/about/about.component';
+import { ContactsComponent } from './ui/main/contacts/contacts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent,
+    HomeComponent,
+    ServicesComponent,
+    AboutComponent,
+    ContactsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatGridListModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
